cc1cf8cec7792c6ac4601214a5bec7bcc174e433
zer0tonin <bouhahah@gmail.com>
2016-09-11 12:35:33 -0400

    Added HS chape to the bullets

--- bullet.lua
+++ bullet.lua
@@ -1,11 +1,13 @@
 local Bullet = {}
+local HC = require "HC"
 
 function Bullet:new(start, newAngle, newImage)
     local newBullet = {
         x = start.x,
         y = start.y,
         angle = newAngle,
-        speed = 250
+        speed = 250,
+        shape = HC.circle(start.x + 11/2, start.y + 11/2, 11/2)
     }
 
     self.__index = self
@@ -18,6 +20,8 @@ function Bullet:move(dt)
 
     self.x = self.x + dx
     self.y = self.y + dy
+
+    self.shape:move(dx, dy)
 end
 
 return Bullet
